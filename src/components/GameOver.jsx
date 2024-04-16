4bb185eec1b7eaca18483247c00e97665a1abb50
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-04-16 10:07:13 +0200

    Final Polishing & Improving Components

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+function GameOver({ winner, onRematch }) {
+  return (
+    <div id="game-over">
+      <h2>Game over!</h2>
+      {winner ? <p>{winner} won!</p> : <p>It's a draw!</p>}
+      <p>
+        <button onClick={onRematch}>Rematch!</button>
+      </p>
+    </div>
+  );
+}
+export default GameOver;
